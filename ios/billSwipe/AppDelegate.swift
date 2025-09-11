f4b62791c8ee2b539dd7e83f575e904768edbde4
Nirav <vavadiya.nirav@gmail.com>
2025-09-11 12:08:37 +0530

    firbase setup done ios

--- ios/billSwipe/AppDelegate.swift
+++ ios/billSwipe/AppDelegate.swift
@@ -2,6 +2,9 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleSignIn
+import FirebaseCore
+import AppAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    FirebaseApp.configure()
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
